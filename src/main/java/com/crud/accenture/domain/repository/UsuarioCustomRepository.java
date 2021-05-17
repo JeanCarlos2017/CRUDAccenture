f63794c3b6a621f317a5d0e9cd1036028fe64580
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-17 17:16:05 -0300

    Create UsuarioCustomRepository.java

---
+++ src/main/java/com/crud/accenture/domain/repository/UsuarioCustomRepository.java
@@ -0,0 +1,84 @@
+package com.crud.accenture.domain.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import com.crud.accenture.domain.model.Usuario;
+
+@Repository
+public class UsuarioCustomRepository {
+	private final EntityManager entityManager;
+	private static final String baseQuery= "select U from Usuario as U where ";
+	private String queryCustom;
+	private String operatorQueryCustom;
+	private TypedQuery<Usuario> queryEntityManager;
+	
+
+	public UsuarioCustomRepository(EntityManager entityManager) {
+		this.entityManager = entityManager;
+		this.queryCustom = UsuarioCustomRepository.baseQuery;
+	}
+	
+	public List<Usuario> findUsuario(String nome, String email){
+		this.createQueryString(nome, email);
+		this.createQuery();
+		this.setParamaterQuery(nome, email);
+		return this.executeQuery();
+	}
+
+	private void createQueryString(String nome, String email) {
+		this.tryAppendNameToQuery(nome);
+		this.tryAppendEmailToQuery(email);
+	}
+
+	private void tryAppendNameToQuery(String nome) {
+		if (nome != null) {
+			this.queryCustom+= "U.nome = :nome ";
+			this.operatorQueryCustom= " and ";
+		}
+	}
+
+	private void tryAppendEmailToQuery(String email) {
+		if (email != null) {
+			this.queryCustom+= this.operatorQueryCustom + "U.email = :email ";
+			this.operatorQueryCustom= " and ";
+		}
+	}
+
+	private void createQuery() {
+		this.queryEntityManager= this.entityManager.createQuery(this.queryCustom, Usuario.class);
+		this.resetQueryCustom();
+	}
+	
+	private void resetQueryCustom() {
+		System.out.println("Query: "+this.queryCustom);
+		this.queryCustom= UsuarioCustomRepository.baseQuery;		
+	}
+
+	private void setParamaterQuery(String nome, String email) {
+		this.setparamaterNome(nome);		
+		this.setparamaterEmail(email);		
+	}
+
+	private void setparamaterNome(String nome) {
+		if (nome != null) {
+		  this.queryEntityManager.setParameter("nome", nome);
+		}
+		
+	}
+
+	private void setparamaterEmail(String email) {
+		if (email != null) {
+			  this.queryEntityManager.setParameter("email", email);
+			}
+	}
+	
+	private List<Usuario> executeQuery() {
+		return this.queryEntityManager.getResultList();
+	}
+	
+}
