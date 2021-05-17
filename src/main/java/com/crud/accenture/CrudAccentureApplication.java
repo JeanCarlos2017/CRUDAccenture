9194bcc4976b6ffc2ff6541f17eabc3e4e82e7f3
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-17 12:53:09 -0300

    first commit

---
+++ src/main/java/com/crud/accenture/CrudAccentureApplication.java
@@ -0,0 +1,13 @@
+package com.crud.accenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAccentureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/accenture/CrudAccentureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.accenture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAccentureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
