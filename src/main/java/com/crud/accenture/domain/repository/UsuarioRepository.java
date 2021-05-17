fed39a90e05450b21c4edfdc54b7c395c58567c8
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-17 20:08:35 -0300

    serviços para usuário

--- src/main/java/com/crud/accenture/domain/repository/UsuarioRepository.java
+++ src/main/java/com/crud/accenture/domain/repository/UsuarioRepository.java
@@ -1,10 +1,14 @@
 package com.crud.accenture.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.crud.accenture.domain.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
 	
+	boolean existsById(int id);
 	
+	Optional<Usuario> findByLoginAndSenha(String nome, String senha);
 }
---
+++ src/main/java/com/crud/accenture/domain/service/UsuarioServiceImpl.java
@@ -0,0 +1,76 @@
+package com.crud.accenture.domain.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crud.accenture.api.exception.ResourceNotFoundException;
+import com.crud.accenture.domain.model.Usuario;
+import com.crud.accenture.domain.repository.UsuarioCustomRepository;
+import com.crud.accenture.domain.repository.UsuarioRepository;
+
+@Service @Transactional
+public class UsuarioServiceImpl implements UsuarioService{
+	
+	private UsuarioRepository usuarioRepository;
+	private UsuarioCustomRepository customRepository;
+	
+	public UsuarioServiceImpl(UsuarioRepository usuarioRepository, UsuarioCustomRepository customRepository) {
+		super();
+		this.usuarioRepository = usuarioRepository;
+		this.customRepository = customRepository;
+	}
+
+	@Override
+	public Usuario cadastraUsuario(Usuario usuario) {
+		return this.usuarioRepository.save(usuario);
+	}
+
+	@Override
+	public Usuario alteraUsuario(Usuario usuario) {
+		if (this.usuarioRepository.existsById(usuario.getId())) {
+			return this.usuarioRepository.save(usuario);
+		}else {
+			return this.throwIdNotFound();
+		}
+	}
+
+	@Override
+	public void deletaUsuario(int idUsuario) {
+		if (this.usuarioRepository.existsById(idUsuario)) {
+			this.usuarioRepository.deleteById(idUsuario);
+		}else {
+			this.throwIdNotFound();
+		}
+	}
+
+	@Override
+	public Usuario buscausuario(int idUsuario) {
+		if(this.usuarioRepository.existsById(idUsuario)) {
+			return this.usuarioRepository.findById(idUsuario).get();
+		}else {
+			return this.throwIdNotFound();
+		}
+	}
+
+	@Override
+	public List<Usuario> filtro(String nome, String email) {
+		return this.customRepository.findUsuario(nome, email);
+	}
+
+	@Override
+	public Usuario login(String login, String senha) {
+		Optional<Usuario> loginUsuario= this.usuarioRepository.findByLoginAndSenha(login, senha);
+		if(loginUsuario.isPresent()) {
+			return loginUsuario.get();
+		}else {
+			throw new ResourceNotFoundException("Login e/ou senha incorreto");
+		}
+	}
+	
+	private Usuario throwIdNotFound() {
+		throw new ResourceNotFoundException("Id do usuário não  encontrado, por favor tente novamente");
+	}
+}
