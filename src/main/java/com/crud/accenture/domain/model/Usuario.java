5d37757733bf40f70b91d5059ea780fcd45b49fd
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-17 15:08:32 -0300

    Create Usuario.java

---
+++ src/main/java/com/crud/accenture/domain/model/Usuario.java
@@ -0,0 +1,122 @@
+package com.crud.accenture.domain.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity @Table(name = "USUARIO")
+public class Usuario {
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro= new java.sql.Date(System.currentTimeMillis());
+	
+	@Column(length = 30)
+	private String nome;
+	
+	@Column(length = 15, unique = true)
+	private String login;
+	
+	@Column(length = 10)
+	private String senha;
+	
+	@Column(length = 11)
+	private String telefone;
+	
+	@Column(length = 100) 
+	private String email;
+	
+	@Column(length = 1)
+	private String perfil;
+	
+	@Column(length = 1)
+	private String status;
+
+	public Usuario() {}
+
+
+	//getters ans setters 
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+	
+	
+}
