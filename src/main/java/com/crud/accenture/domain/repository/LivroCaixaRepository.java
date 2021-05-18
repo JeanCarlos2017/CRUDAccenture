72fdeb4299e87662db7e19b84ff01b2dd831e597
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-18 16:32:32 -0300

    Update LivroCaixaRepository.java

--- src/main/java/com/crud/accenture/domain/repository/LivroCaixaRepository.java
+++ src/main/java/com/crud/accenture/domain/repository/LivroCaixaRepository.java
@@ -1,9 +1,14 @@
 package com.crud.accenture.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.crud.accenture.domain.model.Cliente;
 import com.crud.accenture.domain.model.LivroCaixa;
 
 public interface LivroCaixaRepository extends JpaRepository<LivroCaixa, Integer>{
 	boolean existsById(int id);
+	
+	List<LivroCaixa> findByCliente(Cliente cliente);
 }
