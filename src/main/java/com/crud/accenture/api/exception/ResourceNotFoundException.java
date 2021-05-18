81dd1f039a01224eba03736128d4c05cc1af7553
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-18 10:41:51 -0300

    Update ResourceNotFoundException.java
    add status bad request

--- src/main/java/com/crud/accenture/api/exception/ResourceNotFoundException.java
+++ src/main/java/com/crud/accenture/api/exception/ResourceNotFoundException.java
@@ -1,8 +1,9 @@
 package com.crud.accenture.api.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class ResourceNotFoundException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
