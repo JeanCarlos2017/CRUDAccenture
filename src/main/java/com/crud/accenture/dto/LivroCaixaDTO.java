1ca0539b9a94e0e2a6b0ba269b712c80dfa56de8
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-18 18:05:52 -0300

    Create LivroCaixaDTO.java

---
+++ src/main/java/com/crud/accenture/dto/LivroCaixaDTO.java
@@ -0,0 +1,61 @@
+package com.crud.accenture.dto;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.crud.accenture.domain.model.LivroCaixa;
+
+public class LivroCaixaDTO {
+	private Date dataLancamento;
+	private String descricao;
+	private char tipo;
+	private BigDecimal valor; 
+	private BigDecimal saldo;
+	
+	public LivroCaixaDTO(LivroCaixa livroCaixa) {
+		super();
+		this.dataLancamento = livroCaixa.getDataCadastro();
+		this.descricao = livroCaixa.getDescricao();
+		this.tipo = livroCaixa.getTipo();
+		this.valor = livroCaixa.getValor();
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "LivroCaixaDTO [dataLancamento=" + dataLancamento + ", descricao=" + descricao + ", tipo=" + tipo
+				+ ", valor=" + valor + ", saldo=" + saldo + "]";
+	}
+
+
+	//getters and setters 
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public Date getDataLancamento() {
+		return dataLancamento;
+	}
+
+
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+
+
+	public char getTipo() {
+		return tipo;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	
+}
