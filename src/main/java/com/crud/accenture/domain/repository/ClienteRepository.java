646413996e97fdd5af5f26dcbcbd2b8e208f62c5
Jean Carlos Almeida Correa <jean__100limites@hotmail.com>
2021-05-20 12:01:47 -0300

    Update ClienteRepository.java
    JPA extends JpaSpecificationExecutor para poder trabalhar com o ClienteFilter

--- src/main/java/com/crud/accenture/domain/repository/ClienteRepository.java
+++ src/main/java/com/crud/accenture/domain/repository/ClienteRepository.java
@@ -1,10 +1,11 @@
 package com.crud.accenture.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.crud.accenture.domain.model.Cliente;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>, JpaSpecificationExecutor<Cliente> {
 	boolean existsById(int id);
 	
 }
